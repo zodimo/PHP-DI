c2d5d640136c2f692df71fb02aba02cebea16afb
Jaco Labuschagne <jaco@munsoft.co.za>
2024-09-27 18:22:14 +0000

    more tests

--- src/HKT/HigherKindedContainerBuilder.php
+++ src/HKT/HigherKindedContainerBuilder.php
@@ -60,6 +60,12 @@ class HigherKindedContainerBuilder
     private ?string $compileToDirectory = null;
 
     private bool $sourceCache = false;
+    protected string $sourceCacheNamespace = '';
+
+    /**
+     * Whether the container has already been built.
+     */
+    private bool $locked = false;
 
     /**
      * @param class-string<HigherKindedContainer> $containerClass Name of the container class, used to create the container.
@@ -137,5 +143,170 @@ class HigherKindedContainerBuilder
         }
 
         return new $containerClass($source, $proxyFactory, $this->wrapperContainer);
+    }
+
+    /**
+     * Enables the use of APCu to cache definitions.
+     *
+     * You must have APCu enabled to use it.
+     *
+     * Before using this feature, you should try these steps first:
+     * - enable compilation if not already done (see `enableCompilation()`)
+     * - if you use autowiring or attributes, add all the classes you are using into your configuration so that
+     *   PHP-DI knows about them and compiles them
+     * Once this is done, you can try to optimize performances further with APCu. It can also be useful if you use
+     * `Container::make()` instead of `get()` (`make()` calls cannot be compiled so they are not optimized).
+     *
+     * Remember to clear APCu on each deploy else your application will have a stale cache. Do not enable the cache
+     * in development environment: any change you will make to the code will be ignored because of the cache.
+     *
+     * @see https://php-di.org/doc/performances.html
+     *
+     * @param string $cacheNamespace use unique namespace per container when sharing a single APC memory pool to prevent cache collisions
+     * @return $this
+     */
+    public function enableDefinitionCache(string $cacheNamespace = '') : self
+    {
+        $this->ensureNotLocked();
+
+        $this->sourceCache = true;
+        $this->sourceCacheNamespace = $cacheNamespace;
+
+        return $this;
+    }
+
+    private function ensureNotLocked() : void
+    {
+        if ($this->locked) {
+            throw new \LogicException('The ContainerBuilder cannot be modified after the container has been built');
+        }
+    }
+
+    /**
+     * If PHP-DI's container is wrapped by another container, we can
+     * set this so that PHP-DI will use the wrapper rather than itself for building objects.
+     *
+     * @return $this
+     */
+    public function wrapContainer(HigherKindedContainerInterface $otherContainer) : self
+    {
+        $this->ensureNotLocked();
+
+        $this->wrapperContainer = $otherContainer;
+
+        return $this;
+    }
+
+    /**
+     * Add definitions to the container.
+     *
+     * @param string|array|DefinitionSource ...$definitions Can be an array of definitions, the
+     *                                                      name of a file containing definitions
+     *                                                      or a DefinitionSource object.
+     * @return $this
+     */
+    public function addDefinitions(string|array|DefinitionSource ...$definitions) : self
+    {
+        $this->ensureNotLocked();
+
+        foreach ($definitions as $definition) {
+            $this->definitionSources[] = $definition;
+        }
+
+        return $this;
+    }
+
+    // /**
+    //  * Compile the container for optimum performances.
+    //  *
+    //  * Be aware that the container is compiled once and never updated!
+    //  *
+    //  * Therefore:
+    //  *
+    //  * - in production you should clear that directory every time you deploy
+    //  * - in development you should not compile the container
+    //  *
+    //  * @see https://php-di.org/doc/performances.html
+    //  *
+    //  * @psalm-template T of CompiledContainer
+    //  *
+    //  * @param string $directory Directory in which to put the compiled container.
+    //  * @param string $containerClass Name of the compiled class. Customize only if necessary.
+    //  * @param class-string<Container> $containerParentClass Name of the compiled container parent class. Customize only if necessary.
+    //  * @psalm-param class-string<T> $containerParentClass
+    //  *
+    //  * @psalm-return self<T>
+    //  */
+    // public function enableCompilation(
+    //     string $directory,
+    //     string $containerClass = 'CompiledContainer',
+    //     string $containerParentClass = CompiledContainer::class,
+    // ) : self {
+    //     $this->ensureNotLocked();
+
+    //     $this->compileToDirectory = $directory;
+    //     $this->containerClass = $containerClass;
+    //     $this->containerParentClass = $containerParentClass;
+
+    //     return $this;
+    // }
+
+    /**
+     * Enable or disable the use of autowiring to guess injections.
+     *
+     * Enabled by default.
+     *
+     * @return $this
+     */
+    public function useAutowiring(bool $bool) : self
+    {
+        $this->ensureNotLocked();
+
+        $this->useAutowiring = $bool;
+
+        return $this;
+    }
+
+    /**
+     * Enable or disable the use of PHP 8 attributes to configure injections.
+     *
+     * Disabled by default.
+     *
+     * @return $this
+     */
+    public function useAttributes(bool $bool) : self
+    {
+        $this->ensureNotLocked();
+
+        $this->useAttributes = $bool;
+
+        return $this;
+    }
+
+    /**
+     * Configure the proxy generation.
+     *
+     * For dev environment, use `writeProxiesToFile(false)` (default configuration)
+     * For production environment, use `writeProxiesToFile(true, 'tmp/proxies')`
+     *
+     * @see https://php-di.org/doc/lazy-injection.html
+     *
+     * @param bool $writeToFile If true, write the proxies to disk to improve performances
+     * @param string|null $proxyDirectory Directory where to write the proxies
+     * @return $this
+     * @throws \InvalidArgumentException when writeToFile is set to true and the proxy directory is null
+     */
+    public function writeProxiesToFile(bool $writeToFile, ?string $proxyDirectory = null) : self
+    {
+        $this->ensureNotLocked();
+
+        if ($writeToFile && $proxyDirectory === null) {
+            throw new \InvalidArgumentException(
+                'The proxy directory must be specified if you want to write proxies on disk'
+            );
+        }
+        $this->proxyDirectory = $writeToFile ? $proxyDirectory : null;
+
+        return $this;
     }
 }
--- tests/UnitTest/HKT/HigherKindedContainerBuilderTest.php
+++ tests/UnitTest/HKT/HigherKindedContainerBuilderTest.php
@@ -5,17 +5,18 @@ declare(strict_types=1);
 namespace DI\Test\UnitTest;
 
 use DI\CompiledContainer;
-use DI\ContainerBuilder;
-use DI\Definition\Source\DefinitionArray;
-use DI\Definition\Source\SourceCache;
-use DI\Definition\ValueDefinition;
+
+use DI\HKT\Container\HigherKindedContainerInterface;
+use DI\HKT\Definition\Source\DefinitionArray;
+use DI\HKT\Definition\Source\SourceCache;
+use DI\HKT\Definition\ValueDefinition;
 use DI\HKT\HigherKindedContainerBuilder;
+use DI\HKT\TypeParameters\GenericTypeParameters;
 use DI\Test\IntegrationTest\BaseContainerTest;
 use DI\Test\UnitTest\Fixtures\FakeContainer;
 use DI\Test\UnitTest\Fixtures\FakeHigherKindedContainer;
 use EasyMock\EasyMock;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
 
 /**
  * @covers \DI\ContainerBuilder
@@ -26,6 +27,9 @@ class HigherKindedContainerBuilderTest extends TestCase
 
     private static function getProperty(object $object, string $propertyName)
     {
+        /**
+         * @psalm-suppress PossiblyInvalidFunctionCall
+         */
         return (function (string $propertyName) {
             return $this->$propertyName;
         })->bindTo($object, $object)($propertyName);
@@ -44,121 +48,121 @@ class HigherKindedContainerBuilderTest extends TestCase
         $this->assertNull(self::getProperty($container->proxyFactory, 'proxyDirectory'));
     }
 
-    // public function testShould_allow_to_configure_a_cache()
-    // {
-    //     if (! SourceCache::isSupported()) {
-    //         $this->markTestSkipped('APCu extension is required');
+    public function testShould_allow_to_configure_a_cache()
+    {
+        if (! SourceCache::isSupported()) {
+            $this->markTestSkipped('APCu extension is required');
 
-    //         return;
-    //     }
+            return;
+        }
 
-    //     $builder = new ContainerBuilder(FakeContainer::class);
-    //     $builder->enableDefinitionCache();
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
+        $builder->enableDefinitionCache();
 
-    //     /** @var FakeContainer $container */
-    //     $container = $builder->build();
+        /** @var FakeContainer $container */
+        $container = $builder->build();
 
-    //     $this->assertInstanceOf(SourceCache::class, $container->definitionSource);
-    // }
+        $this->assertInstanceOf(SourceCache::class, $container->definitionSource);
+    }
 
-    // public function testShould_allow_to_configure_a_cache_with_a_namespace()
-    // {
-    //     if (! SourceCache::isSupported()) {
-    //         $this->markTestSkipped('APCu extension is required');
+    public function testShould_allow_to_configure_a_cache_with_a_namespace()
+    {
+        if (! SourceCache::isSupported()) {
+            $this->markTestSkipped('APCu extension is required');
 
-    //         return;
-    //     }
+            return;
+        }
 
-    //     $namespace = 'staging';
-    //     $builder = new ContainerBuilder(FakeContainer::class);
-    //     $builder->enableDefinitionCache($namespace);
+        $namespace = 'staging';
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
+        $builder->enableDefinitionCache($namespace);
 
-    //     /** @var FakeContainer $container */
-    //     $container = $builder->build();
-    //     $source = $container->definitionSource;
+        /** @var FakeContainer $container */
+        $container = $builder->build();
+        $source = $container->definitionSource;
 
-    //     $this->assertInstanceOf(SourceCache::class, $source);
-    //     $this->assertSame($source->getCacheKey('foo'), SourceCache::CACHE_KEY . $namespace . 'foo');
-    // }
+        $this->assertInstanceOf(SourceCache::class, $source);
+        $this->assertSame($source->getCacheKey('foo'), SourceCache::CACHE_KEY . $namespace . 'foo');
+    }
 
-    // public function testThe_container_should_not_be_wrapped_by_default()
-    // {
-    //     $builder = new ContainerBuilder(FakeContainer::class);
-    //     /** @var FakeContainer $container */
-    //     $container = $builder->build();
+    public function testThe_container_should_not_be_wrapped_by_default()
+    {
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
+        /** @var FakeHigherKindedContainer $container */
+        $container = $builder->build();
 
-    //     $this->assertNull($container->wrapperContainer);
-    // }
+        $this->assertNull($container->wrapperContainer);
+    }
 
-    // public function testShould_allow_to_set_a_wrapper_container()
-    // {
-    //     $otherContainer = $this->easyMock(ContainerInterface::class);
+    public function testShould_allow_to_set_a_wrapper_container()
+    {
+        $otherContainer = $this->easyMock(HigherKindedContainerInterface::class);
 
-    //     $builder = new ContainerBuilder(FakeContainer::class);
-    //     $builder->wrapContainer($otherContainer);
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
+        $builder->wrapContainer($otherContainer);
 
-    //     /** @var FakeContainer $container */
-    //     $container = $builder->build();
+        /** @var FakeContainer $container */
+        $container = $builder->build();
 
-    //     $this->assertSame($otherContainer, $container->wrapperContainer);
-    // }
+        $this->assertSame($otherContainer, $container->wrapperContainer);
+    }
 
-    // public function testShould_allow_to_add_custom_definition_sources()
-    // {
-    //     $builder = new ContainerBuilder(FakeContainer::class);
+    public function testShould_allow_to_add_custom_definition_sources()
+    {
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
 
-    //     // Custom definition sources should be chained correctly
-    //     $builder->addDefinitions(new DefinitionArray(['foo' => 'bar']), new DefinitionArray(['foofoo' => 'barbar']));
+        // Custom definition sources should be chained correctly
+        $builder->addDefinitions(new DefinitionArray(['foo' => 'bar']), new DefinitionArray(['foofoo' => 'barbar']));
 
-    //     /** @var FakeContainer $container */
-    //     $container = $builder->build();
+        /** @var FakeHigherKindedContainer $container */
+        $container = $builder->build();
 
-    //     // We should be able to get entries from our custom definition sources
-    //     /** @var ValueDefinition $definition */
-    //     $definition = $container->definitionSource->getDefinition('foo');
-    //     $this->assertInstanceOf(ValueDefinition::class, $definition);
-    //     $this->assertSame('bar', $definition->getValue());
-    //     $definition = $container->definitionSource->getDefinition('foofoo');
-    //     $this->assertInstanceOf(ValueDefinition::class, $definition);
-    //     $this->assertSame('barbar', $definition->getValue());
-    // }
+        // We should be able to get entries from our custom definition sources
+        /** @var ValueDefinition $definition */
+        $definition = $container->definitionSource->getDefinition('foo', GenericTypeParameters::createEmpty());
+        $this->assertInstanceOf(ValueDefinition::class, $definition);
+        $this->assertSame('bar', $definition->getValue());
+        $definition = $container->definitionSource->getDefinition('foofoo', GenericTypeParameters::createEmpty());
+        $this->assertInstanceOf(ValueDefinition::class, $definition);
+        $this->assertSame('barbar', $definition->getValue());
+    }
 
-    // public function testShould_chain_definition_sources_in_reverse_order()
-    // {
-    //     $builder = new ContainerBuilder(FakeContainer::class);
+    public function testShould_chain_definition_sources_in_reverse_order()
+    {
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
 
-    //     $builder->addDefinitions(new DefinitionArray(['foo' => 'bar']), new DefinitionArray(['foo' => 'bim']));
+        $builder->addDefinitions(new DefinitionArray(['foo' => 'bar']), new DefinitionArray(['foo' => 'bim']));
 
-    //     /** @var FakeContainer $container */
-    //     $container = $builder->build();
+        /** @var FakeHigherKindedContainer $container */
+        $container = $builder->build();
 
-    //     /** @var ValueDefinition $definition */
-    //     $definition = $container->definitionSource->getDefinition('foo');
-    //     $this->assertSame('bim', $definition->getValue());
-    // }
+        /** @var ValueDefinition $definition */
+        $definition = $container->definitionSource->getDefinition('foo', GenericTypeParameters::createEmpty());
+        $this->assertSame('bim', $definition->getValue());
+    }
 
-    // public function testShould_allow_to_add_definitions_in_an_array()
-    // {
-    //     $builder = new ContainerBuilder(FakeContainer::class);
+    public function testShould_allow_to_add_definitions_in_an_array()
+    {
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
 
-    //     // Custom definition sources should be chained correctly
-    //     $builder->addDefinitions(['foo' => 'bar'], ['foofoo' => 'barbar']);
+        // Custom definition sources should be chained correctly
+        $builder->addDefinitions(['foo' => 'bar'], ['foofoo' => 'barbar']);
 
-    //     /** @var FakeContainer $container */
-    //     $container = $builder->build();
+        /** @var FakeHigherKindedContainer $container */
+        $container = $builder->build();
 
-    //     /** @var ValueDefinition $definition */
-    //     $definition = $container->definitionSource->getDefinition('foo');
-    //     $this->assertInstanceOf(ValueDefinition::class, $definition);
-    //     $this->assertSame('bar', $definition->getValue());
-    //     $definition = $container->definitionSource->getDefinition('foofoo');
-    //     $this->assertInstanceOf(ValueDefinition::class, $definition);
-    //     $this->assertSame('barbar', $definition->getValue());
-    // }
+        /** @var ValueDefinition $definition */
+        $definition = $container->definitionSource->getDefinition('foo', GenericTypeParameters::createEmpty());
+        $this->assertInstanceOf(ValueDefinition::class, $definition);
+        $this->assertSame('bar', $definition->getValue());
+        $definition = $container->definitionSource->getDefinition('foofoo', GenericTypeParameters::createEmpty());
+        $this->assertInstanceOf(ValueDefinition::class, $definition);
+        $this->assertSame('barbar', $definition->getValue());
+    }
 
     // public function testShould_allow_to_create_a_compiled_container()
     // {
-    //     $builder = new ContainerBuilder();
+    //     $builder = new HigherKindedContainerBuilder();
     //     $builder->enableCompilation(BaseContainerTest::COMPILATION_DIR);
 
     //     $this->assertInstanceOf(CompiledContainer::class, $builder->build());
@@ -177,66 +181,68 @@ class HigherKindedContainerBuilderTest extends TestCase
     //     $this->assertInstanceOf($className, $builder->build());
     // }
 
-    // public function testShould_have_a_fluent_interface()
-    // {
-    //     $builder = new ContainerBuilder();
+    public function testShould_have_a_fluent_interface()
+    {
+        $builder = new HigherKindedContainerBuilder();
 
-    //     $result = $builder->useAttributes(false);
-    //     $this->assertSame($builder, $result);
+        $result = $builder->useAttributes(false);
+        $this->assertSame($builder, $result);
 
-    //     $result = $builder->useAttributes(true);
-    //     $this->assertSame($builder, $result);
+        $result = $builder->useAttributes(true);
+        $this->assertSame($builder, $result);
 
-    //     $result = $builder->useAutowiring(false);
-    //     $this->assertSame($builder, $result);
+        $result = $builder->useAutowiring(false);
+        $this->assertSame($builder, $result);
 
-    //     $result = $builder->useAutowiring(true);
-    //     $this->assertSame($builder, $result);
+        $result = $builder->useAutowiring(true);
+        $this->assertSame($builder, $result);
 
-    //     $result = $builder->writeProxiesToFile(true, 'somedir');
-    //     $this->assertSame($builder, $result);
+        $result = $builder->writeProxiesToFile(true, 'somedir');
+        $this->assertSame($builder, $result);
 
-    //     $result = $builder->writeProxiesToFile(false);
-    //     $this->assertSame($builder, $result);
+        $result = $builder->writeProxiesToFile(false);
+        $this->assertSame($builder, $result);
 
-    //     $result = $builder->enableCompilation('foo');
-    //     $this->assertSame($builder, $result);
+        // $result = $builder->enableCompilation('foo');
+        // $this->assertSame($builder, $result);
 
-    //     $result = $builder->enableDefinitionCache();
-    //     $this->assertSame($builder, $result);
+        $result = $builder->enableDefinitionCache();
+        $this->assertSame($builder, $result);
 
-    //     $result = $builder->wrapContainer($this->easyMock(ContainerInterface::class));
-    //     $this->assertSame($builder, $result);
-    // }
+        $result = $builder->wrapContainer($this->easyMock(HigherKindedContainerInterface::class));
+        $this->assertSame($builder, $result);
+    }
 
-    // /**
-    //  * Ensure the ContainerBuilder cannot be modified after the container has been built.
-    //  */
-    // public function testShould_throw_if_modified_after_building_a_container()
-    // {
-    //     $this->expectException('LogicException');
-    //     $this->expectExceptionMessage('The ContainerBuilder cannot be modified after the container has been built');
-    //     $builder = new ContainerBuilder();
-    //     $builder->build();
+    /**
+     * Ensure the ContainerBuilder cannot be modified after the container has been built.
+     */
+    public function testShould_throw_if_modified_after_building_a_container()
+    {
+        $this->expectException('LogicException');
+        $this->expectExceptionMessage('The ContainerBuilder cannot be modified after the container has been built');
+        $builder = new HigherKindedContainerBuilder();
+        $builder->build();
 
-    //     $builder->addDefinitions([]);
-    // }
+        $builder->addDefinitions([]);
+    }
 
-    // public function testShould_create_proxies()
-    // {
-    //     $builder = new ContainerBuilder(FakeContainer::class);
-    //     $builder->writeProxiesToFile(true, 'somedir');
-    //     $container = $builder->build();
+    public function testShould_create_proxies()
+    {
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
 
-    //     $this->assertSame('somedir', self::getProperty($container->proxyFactory, 'proxyDirectory'));
-    // }
+        $builder->writeProxiesToFile(true, 'somedir');
+        $container = $builder->build();
 
-    // public function testShould_not_create_proxies()
-    // {
-    //     $builder = new ContainerBuilder(FakeContainer::class);
-    //     $builder->writeProxiesToFile(false, 'somedir');
-    //     $container = $builder->build();
+        $this->assertSame('somedir', self::getProperty($container->proxyFactory, 'proxyDirectory'));
+    }
 
-    //     $this->assertNull(self::getProperty($container->proxyFactory, 'proxyDirectory'));
-    // }
+    public function testShould_not_create_proxies()
+    {
+        $builder = new HigherKindedContainerBuilder(FakeHigherKindedContainer::class);
+
+        $builder->writeProxiesToFile(false, 'somedir');
+        $container = $builder->build();
+
+        $this->assertNull(self::getProperty($container->proxyFactory, 'proxyDirectory'));
+    }
 }
